1213bbee263a80d94908c2a512f629ef22d6e21b
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-21 15:58:39 +0500

    Initial Commit

---
+++ CustomGrid/CustomGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridApp.swift
+//  CustomGrid
+//
+//  Created by Arkasha Zuev on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
